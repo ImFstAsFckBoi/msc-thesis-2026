f7f48bd8b183fc43e34604f1fb14ccd35b52feb1
Martin Olivesten <marol450@student.liu.se>
2026-02-19 09:15:18 +0100

    Started on Parikh function

---
+++ flat/flat/parikh.py
@@ -0,0 +1,34 @@
+from  flat.grammar import EPSILON, Grammar, NTerm, Production, TTerm
+
+
+def myster_M(G: Grammar):
+    pass
+
+def parikh(G: Grammar):
+    formula: list[str] = []
+    for N in G.nonterms:
+        clause = f"M_G({N})"
+        for i, p in enumerate(G.prods):
+            if p.rhs.count(N) > 0:
+                clause += f"+{p.rhs.count(N)}*y(p{i+1})"
+        formula.append(f"({clause}=0)")
+    
+    for a in G.terms:
+        sums: list[str] = []
+        for i, p in enumerate(G.prods):
+            sums.append(f"{p.rhs.count(a)}*y(p{i+1})")
+        formula.append(f"x{a}=" + "+".join(sums))
+    return " AND ".join(formula)
+
+if __name__ == "__main__":
+    
+    S = NTerm("S")
+    Sp = NTerm("S'")
+    a, b, c, d = map(TTerm, ("a", "b", "c", "d"))
+    p1 = Production(S, a, a, S, b)
+    p2 = Production(S, Sp)
+    p3 = Production(Sp, c, Sp, d)
+    p4 = Production(Sp, EPSILON)
+    G = Grammar([S, Sp], [a, b, c, d, EPSILON], [p1, p2, p3, p4], S)
+
+    print(parikh(G))
